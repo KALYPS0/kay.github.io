ffd610301b3a8b9e923f15a2cbd247b031a032d4
Kay Rose <45441563+KALYPS0@users.noreply.github.com>
2022-08-08 23:48:24 -0700

    adding the welcome text to the main page, adding the hover listening functions to change the font color when the mouse is hovering, creating the bounce animation to the welcome text on the main page

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // Want something
+const welcome_text = document.querySelector("#welcome");
+welcome_text.addEventListener("mouseover", hoverOverWelcome);
+welcome_text.addEventListener("mouseout", hoverEnd);
+
+function hoverOverWelcome(event) {
+  console.log("Hovering over the welcome text.\n");
+  welcome_text.setAttribute("style", "color: red");
+}
+
+function hoverEnd(event) {
+  console.log("Not hovering anymore.\n");
+  welcome_text.setAttribute("style", "color: black");
+}
