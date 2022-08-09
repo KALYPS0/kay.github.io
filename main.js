5d12f5a3300436f890ac93c16874f0b615186c37
KALYPS0 <ballerinaonhorseback@gmail.com>
2022-08-09 16:09:21 -0700

    update main.js
    changed the main page welcome text hover effect to invert the text and background when hovering over

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ welcome_text.addEventListener("mouseout", hoverEnd);
 
 function hoverOverWelcome(event) {
   console.log("Hovering over the welcome text.\n");
-  welcome_text.setAttribute("style", "color: red");
+  welcome_text.setAttribute("style", "color: white");
+  document.body.style.backgroundColor = "black";
 }
 
 function hoverEnd(event) {
   console.log("Not hovering anymore.\n");
   welcome_text.setAttribute("style", "color: black");
+  document.body.style.backgroundColor = "white";
 }
